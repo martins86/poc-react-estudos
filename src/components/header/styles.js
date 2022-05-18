0a1c25cf212fb4ad561b8eecef42da2f73b967dd
martins86 <martins86@gmail.com>
2022-05-18 10:20:33 -0300

    alterando backgound para gradiente

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -7,7 +7,12 @@ export const Container = styled.div`
     align-items: center;
     padding: 15px 24px;
     width: 100%;
-    background-color: #29ab87;
+    /* Permalink - use to edit and share this gradient: https://colorzilla.com/gradient-editor/#b4df5b+47,f2f2f2+100 */
+    background: #b4df5b; /* Old browsers */
+    background: -moz-linear-gradient(-45deg,  #b4df5b 47%, #f2f2f2 100%); /* FF3.6-15 */
+    background: -webkit-linear-gradient(-45deg,  #b4df5b 47%,#f2f2f2 100%); /* Chrome10-25,Safari5.1-6 */
+    background: linear-gradient(135deg,  #b4df5b 47%,#f2f2f2 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#b4df5b', endColorstr='#f2f2f2',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
 `;
 
 export const Header = styled.h1``;
